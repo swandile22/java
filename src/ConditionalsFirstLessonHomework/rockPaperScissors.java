5c5c7905c9732eb5d66ac09911b5b7de64e0bb6b
swandile22 <sandile@spitfireinbound.com>
2024-02-29 21:39:52 +0200

    Created Slightly cleaner Rock, Paper, Scissors game

--- src/ConditionalsFirstLessonHomework/rockPaperScissors.java
+++ src/ConditionalsFirstLessonHomework/rockPaperScissors.java
@@ -9,46 +9,22 @@ public class rockPaperScissors {
         double randomNum = Math.floor(Math.random() * 3 + 1);
         int newRandomNum = (int) randomNum;
 
-        System.out.println("Please enter a number between 1 and 3");
+        System.out.println("Let's play Rock, Paper, Scissors!");
+        System.out.println("Enter 1 for Rock, 2 for Paper, or 3 for Scissors:");
         Scanner input = new Scanner(System.in);
         int userInput = input.nextInt();
 
-//      Winner Moves
-        if (userInput == 1 && newRandomNum == 3) {
-            System.out.println(newRandomNum);
-            System.out.println("Nice one!, you're a winner!");
-        }
-
-        if (userInput == 2 && newRandomNum == 1) {
-            System.out.println(newRandomNum);
-            System.out.println("Nice one!, you're a winner!");
-        }
-
-        if (userInput == 3 && newRandomNum == 2) {
-            System.out.println(newRandomNum);
-            System.out.println("Nice one!, you're a winner!");
-        }
-
-//      Loser Moves
-        if (userInput == 3 && newRandomNum == 1) {
-            System.out.println(newRandomNum);
-            System.out.println("You lose!, better luck next time!");
-        }
-
-        if (userInput == 1 && newRandomNum == 2) {
-            System.out.println(newRandomNum);
-            System.out.println("You lose!, better luck next time!");
-        }
-
-        if (userInput == 2 && newRandomNum == 3) {
-            System.out.println(newRandomNum);
-            System.out.println("You lose!, better luck next time!");
-        }
-
-//      Draw Move
         if (userInput == newRandomNum) {
-            System.out.println(newRandomNum);
+            System.out.println("Computer chose: " + newRandomNum);
             System.out.println("It's a tie!");
+        } else if ((userInput == 1 && newRandomNum == 3) ||
+                (userInput == 2 && newRandomNum == 1) ||
+                (userInput == 3 && newRandomNum == 2)) {
+            System.out.println("Computer chose: " + newRandomNum);
+            System.out.println("Congratulation! You win!");
+        } else {
+            System.out.println("Computer chose: " + newRandomNum);
+            System.out.println("Sorry, you lose!, better luck next time!");
         }
 
 
