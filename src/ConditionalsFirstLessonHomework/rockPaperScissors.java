7ae934fc271ead8e0f33e1df13846ec8191013c3
swandile22 <sandile@spitfireinbound.com>
2024-02-29 21:45:49 +0200

    Created Slightly cleaner Rock, Paper, Scissors game

--- src/ConditionalsFirstLessonHomework/rockPaperScissors.java
+++ src/ConditionalsFirstLessonHomework/rockPaperScissors.java
@@ -6,17 +6,21 @@ public class rockPaperScissors {
 
     public static void main(String[] args) {
 
+//      Random number
         double randomNum = Math.floor(Math.random() * 3 + 1);
         int newRandomNum = (int) randomNum;
 
+//      User input
         System.out.println("Let's play Rock, Paper, Scissors!");
         System.out.println("Enter 1 for Rock, 2 for Paper, or 3 for Scissors:");
         Scanner input = new Scanner(System.in);
         int userInput = input.nextInt();
 
+//      Conditionals
         if (userInput < 1 || userInput > 3){
             System.out.println("Invalid value");return;
         }
+
         if (userInput == newRandomNum) {
             System.out.println("Computer chose: " + newRandomNum);
             System.out.println("It's a tie!");
