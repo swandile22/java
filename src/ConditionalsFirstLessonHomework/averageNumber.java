d23981744d8e9b3f38272578959e22d8b3824267
swandile22 <sandile@spitfireinbound.com>
2024-03-01 08:13:58 +0200

    Created average number file

---
+++ src/ConditionalsFirstLessonHomework/averageNumber.java
@@ -0,0 +1,18 @@
+package ConditionalsFirstLessonHomework;
+
+import java.util.Scanner;
+
+public class averageNumber {
+
+    public static void main(int[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        while (true) {
+             int userInput = input.nextInt();
+            if (userInput == 0) {
+                break;
+            }
+        }
+    }
+}
