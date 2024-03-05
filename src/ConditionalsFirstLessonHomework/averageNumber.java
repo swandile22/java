fb8a6636e98578d4091b74ab607f3c16f5b36119
swandile22 <sandile@spitfireinbound.com>
2024-03-05 18:19:05 +0200

    Added positive number function

--- src/ConditionalsFirstLessonHomework/averageNumber.java
+++ src/ConditionalsFirstLessonHomework/averageNumber.java
@@ -1,3 +1,12 @@
+/*
+* ArrayList<Type> name = new ArrayList<Type>() - [Class]this is used to create a new array list
+* Scanner input = new Scanner(System.in) - [Class]this is created to take in a user input using(System.in)
+* int userInput = input.nextInt() - declares the user input in a variable
+* break - Often used in loops, this is used to terminate a loop and move on to the code following the loop
+* .add - a method used to add data into a Set or an Array
+*
+* */
+
 package ConditionalsFirstLessonHomework;
 
 import java.util.ArrayList;
@@ -19,7 +28,8 @@ public class averageNumber {
         }
 
         averageSum(numbersList);
-        ArrayList<Integer> negativeNums = negativeNum(numbersList);
+        negativeNum(numbersList);
+        positiveNUm(numbersList);
     }
 
     private static double averageSum(ArrayList<Integer> numbersList) {
@@ -47,10 +57,30 @@ public class averageNumber {
             }
         }
 
-        System.out.println(negList);
+//        System.out.println(negList);
         System.out.println("There are " + negList.size() + " negative numbers");
         return negList;
     }
+
+    private static ArrayList<Integer> positiveNUm(ArrayList<Integer> numbersList) {
+
+        ArrayList<Integer> posList = new ArrayList<>();
+        int posCount = 0;
+
+        for (int i = 0; i < numbersList.size(); i++) {
+            int posNum = numbersList.get(i);
+            if (posNum > 0) {
+                posList.add(posNum);
+            }
+        }
+
+//        System.out.println(posList);
+        System.out.println("There are " + posList.size() + " positive numbers");
+        return posList;
+
+    }
+
+
 }
 
 
