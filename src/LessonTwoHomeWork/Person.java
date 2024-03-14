f7eafa90d864a7ac01828c060d87577d9279356d
swandile22 <sandile@spitfireinbound.com>
2024-03-14 22:05:42 +0200

    🚧 Created Person package

---
+++ src/LessonTwoHomeWork/Person.java
@@ -0,0 +1,22 @@
+package LessonTwoHomeWork;
+
+public class Person {
+
+    private int ID;
+
+    private int projectNumber;
+
+    private String firstName;
+
+    private String lastName;
+
+    private int Contact;
+
+    private String emailAddress;
+
+    private String physicalAddress;
+
+
+
+}
+
